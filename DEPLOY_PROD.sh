dfeca904ca01741c1f9247818b3039879a928e15
cheesestringhustler <manuel8ld@gmail.com>
2024-10-25 23:43:19 +0200

    dev

--- DEPLOY_PROD.sh
+++ DEPLOY_PROD.sh
@@ -6,12 +6,12 @@ source .env
 echo "Starting deployment process..."
 
 echo "Building Docker image..."
-docker build --platform linux/arm64 -t satz.li:latest .
+docker build --platform linux/arm64 -t web:latest .
 
 echo "Saving and transferring Docker image to server..."
-docker save satz.li:latest | gzip | ssh root@$SERVER_IP docker load
+docker save web:latest | gzip | ssh root@$SERVER_IP docker load
 
 echo "Starting the application on the server..."
-ssh root@$SERVER_IP docker compose up -d satz.li
+ssh root@$SERVER_IP docker compose up -d web
 
 echo "Deployment completed successfully!"
